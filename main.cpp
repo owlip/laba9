da919ea8fae6f38563accb190f8fc5a3bddb5763
owlip <danilevich2002@gmail.com>
2021-05-18 11:38:58 +0300

    добавил вывод суммы 2 чисел в main.cpp

--- main.cpp
+++ main.cpp
@@ -6,10 +6,16 @@ int main()
 { 
     setlocale(LC_ALL, "Rus");
  
-    int number ;
-    cout << "Введите целое число: ";
-    cin >> number;
-    
+    int number1 ;
+    int number2 ;
+    cout << "Введите целое число 1: ";
+    cin >> number1;
+    cout << "Введите целое число 2: ";
+    cin >> number2;
+
+    cout << "Сумма = " << number1 + number2 << endl;
+ 
+    getchar();
     getchar();
     return 0;	
 }
